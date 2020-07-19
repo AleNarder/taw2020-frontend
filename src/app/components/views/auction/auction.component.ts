9b2053a6d137fe54a0058fb0a43e0a2d643c1b96
Alessio Narder <alessio.narder@gmail.com>
2020-07-20 00:17:37 +0200

    Invio messaggi terminato

--- src/app/components/views/auction/auction.component.ts
+++ src/app/components/views/auction/auction.component.ts
@@ -5,6 +5,9 @@ import { appStateService } from 'src/app/services/state/appState.service';
 import { Response } from 'src/app/services/models/Response'
 import { Auction } from 'src/app/services/models/Auction'
 import { ChatConfiguration } from 'src/app/models/chat';
+import { SocketioService } from 'src/app/services/socket/socketio.service';
+import { Message } from 'src/app/services/models/Message';
+
 
 @Component({
   selector: 'app-auction',
@@ -18,19 +21,23 @@ export class AuctionComponent implements OnInit {
   userId
   ready = false
   chats: ChatConfiguration[] = []
+  private owner: boolean
 
   constructor(
     private route: ActivatedRoute,
     private auctionService: AuctionService,
     private appState: appStateService,
-  ) {}
+    private socketService: SocketioService
+  ) {
+  }
 
   ngOnInit() {
     this.route.queryParams.subscribe((params) => {
       this.auctionId = params.auction
       this.userId = params.user
+      this.owner = this.appState.state.user._id === this.userId
+      this.getAuctionInfo()
     })
-    this.getAuctionInfo()
   }
 
   getAuctionInfo () {
@@ -38,16 +45,32 @@ export class AuctionComponent implements OnInit {
       this.auction = auction.payload
       this.ready = true
       this.getChatConfig()
+      this.socketService.onNewPrivateMessage(this.updateChats.bind(this))
       console.log(this.auction)
     }, (error) => {
       console.log(error)
     })
   }
 
+  private updateChats (message: Message) {
+    const chat = this.chats.filter((chat) => chat.scope === 'private').find((chat) => chat.receiverId == message.senderId)
+    if (!chat && this.owner) {
+      this.chats.push({
+        scope: 'private',
+        messages: [message],
+        auctionId: this.auctionId,
+        senderId: this.appState.state.user._id,
+        senderUs: this.appState.state.user.username,
+        receiverId: message.senderId,
+        receiverUs: message.senderUs
+      })
+    }
+  }
+
 
   getChatConfig () {
     if (this.appState.state.user) {
-      const owner = this.appState.state.user._id === this.userId
+      if (this.owner) this.auction.chats.filter((chat) => chat.scope === 'public' || (chat.scope === 'private' && chat.partnerId === this.appState.state.user._id))
       for (let chat of this.auction.chats) {
         this.chats.push({
           scope: chat.scope,
@@ -55,11 +78,11 @@ export class AuctionComponent implements OnInit {
           auctionId: this.auctionId,
           senderId: this.appState.state.user._id,
           senderUs: this.appState.state.user.username,
-          receiverId: (chat.scope === 'private' ) ? (owner ? chat.partnerId : this.userId) : this.userId,
-          receiverUs: (chat.scope === 'private' ) ? chat.partnerUs : null
+          receiverId: (chat.scope === 'private' ) ? (this.owner ? chat.partnerId : this.userId) : this.userId,
+          receiverUs: (chat.scope === 'private' ) ? (this.owner ? chat.partnerUs : 'Inserzionista') : null
         })
       }
-      if (this.chats.filter(chat => chat.scope === 'private').length === 0 && !owner) {
+      if (this.chats.filter(chat => chat.scope === 'private').length === 0 && !this.owner) {
         this.chats.push({
           scope: 'private',
           messages: [],
