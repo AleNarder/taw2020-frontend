b0db071e4a224bc7c7da469e6ee850a723cf4973
Alessio Narder <alessio.narder@gmail.com>
2020-08-10 18:55:31 +0200

    cors

--- server.js
+++ server.js
@@ -2,8 +2,11 @@
 const express = require('express');
 const path = require('path');
 const port = process.env.PORT || 8080
+const cors = require('cors')
 
 const app = express();
+
+app.use(cors())
 
 // Serve only the static files form the dist directory
 app.use(express.static(__dirname + '/www'));
